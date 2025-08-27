2ebcecd1c1df645885247f7163c575cb8805e5c2
josem <josem@DESKTOP-4LGULR3>
2025-08-27 17:52:32 -0600

    feat: implementando funciones de editar y eliminar, buscar por Id y editar por Id

---
+++ proyectos/Proyectos .NET/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepo _productRepo;
+
+        public ProductController()
+        {
+            _productRepo = new ProductRepo();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            List<Product> products = await _productRepo.GetAllProductsAsync();
+            return View(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _productRepo.AddProductAsync(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int id)
+        {
+            Product productToEdit = await _productRepo.GetAllProductsAsync(id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            } 
+
+            return View(productToEdit);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditFound(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productRepo.EditProductAsync(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
--- proyectos/Proyectos .NET/WebApplication1/Repositories/ProductRepo.cs
+++ proyectos/Proyectos .NET/WebApplication1/Repositories/ProductRepo.cs
@@ -1,27 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 using WebApplication1.Models;
-
+using System.Linq;
+using System.Data;
+using Microsoft.Ajax.Utilities;
+using System.Web.Mvc;
 
 namespace WebApplication1.Repositories
 {
     public class ProductRepo
     {
-        private string ConnectionString = ConfigurationManager.ConnectionStrings["ProductInventoryDB"].ConnectionString;
+        private readonly string ConnectionString = ConfigurationManager.ConnectionStrings["ProductInventoryDB"].ConnectionString;
 
-        public List<Product> GetAllProducts()
+        public async Task<List<Product>> GetAllProductsAsync()
         {
             List<Product> products = new List<Product>();
 
             using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
-                conn.Open();
+                await conn.OpenAsync();
 
                 string query = "SELECT Id, Name, Quantity, Price FROM Products";
                 using (SqlCommand command = new SqlCommand(query, conn))
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
                     {
                         while (reader.Read())
                         {
@@ -40,11 +44,11 @@ namespace WebApplication1.Repositories
             return products;
         }
 
-        public void AddProduct(Product product)
+        public async Task AddProductAsync(Product product)
         {
             using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
-                conn.Open();
+                await conn.OpenAsync();
 
                 string InsertQuery = "INSERT INTO Products (Name, Quantity, Price) VALUES (@name, @quantity, @price)";
                 using (SqlCommand command = new SqlCommand(InsertQuery, conn))
@@ -52,9 +56,63 @@ namespace WebApplication1.Repositories
                     command.Parameters.AddWithValue("@name", product.Name);
                     command.Parameters.AddWithValue("@quantity", product.Quantity);
                     command.Parameters.AddWithValue("@price", product.Price);
-                    command.ExecuteNonQuery();
+                    await command.ExecuteNonQueryAsync();
                 }
 
+            }
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            Product product = null;
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                await conn.OpenAsync();
+                string IdQuery = "SELECT Id, Name, Quantity, Pirce " +
+                    "FROM Products WHERE Id = @Id;";
+                using (SqlCommand command =  new SqlCommand(IdQuery, conn))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    using (SqlDataReader reader =  await command.ExecuteReaderAsync())
+                    {
+                        if(reader.Read())
+                        {
+                            product = new Product
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                                Price = reader.GetDecimal(reader.GetOrdinal("Price"))
+                            };
+                        }
+                    }
+                }
+            }
+            return product;
+        }
+
+        public async Task<int> EditProductAsync(Product product)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                await conn.OpenAsync();
+                
+                string UpdateQuery = "UPDATE Products " +
+                    "SET Name = @Name, " +
+                    "Quantity = @Quantity, " +
+                    "Price = @Price " +
+                    "WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(UpdateQuery, conn))
+                {
+                    command.Parameters.AddWithValue("@Name", product.Name);
+                    command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                    command.Parameters.AddWithValue("@Price", product.Price);
+                    command.Parameters.AddWithValue("@Id", product.Id);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    return rowsAffected;
+                }
             }
         }
     }
